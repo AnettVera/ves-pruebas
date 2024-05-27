141f5dbce7f0eaae202e391fc638c9ccbb58c72e
MartinOM321753 <20223tn139@utez.edu.mx>
2024-05-27 00:28:27 -0600

    DB

--- client/client/src/config/context/auth-context.js
+++
@@ -1,8 +0,0 @@
-import { createContext } from "react";
-
-// User signed - null
-// user - dispatch
-// Dispatch se refiere a que se hará un cambio
-const AuthContext = createContext();
-
-export default AuthContext;
